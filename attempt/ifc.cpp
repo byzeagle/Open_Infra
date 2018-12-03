0189b84c67269323d2633b641c0a8709a1c9d4a6
Ugur <ugurbd@gmail.com>
2018-12-03 04:06:55 +0100

    Update ifc.cpp

--- attempt/ifc.cpp
+++ attempt/ifc.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <iostream>
 #include "IfcBSplineCurve.hpp"
-#include "type.hpp"
 #include "variant.hpp"
 
 //using boost::variant;
@@ -10,22 +9,6 @@ using std::cout;
 using std::endl;
 using std::ostream;
 
-//enum Version : int {IFC_1, IFC_2, IFC_3, Common};
-
-std::string display(Version v)
-{
-	switch (v)
-	{
-	case IFC_1:
-		return "IFC_1"; 
-	case IFC_2:
-		return "IFC_2"; 
-	case IFC_3:
-		return "IFC_3"; 
-	case Common:
-		return "SuperSet"; 
-	}
-}
 
 int main()
 {
@@ -40,6 +23,8 @@ int main()
 	//ifc2 = ifc1;  // Error
 
 	IfcBSplineCurve<IFC_3> ifc3;
+	ifc3.Degree = 10;
+	ifc3.SelfIntersect = ifclogical();
 
 	IfcBSplineCurve<Common> superset;
 
@@ -49,6 +34,7 @@ int main()
 	superset = ifc1;
 	superset = ifc3;
 	superset = ifc2;
+	superset = ifc3;
 
 	// IFC1 IfcBSpline is type of int
 	int y = ifc1.Degree;
@@ -58,12 +44,11 @@ int main()
 
 	// SuperSet works for both types - because union of Degree is int and ifcinteger
 	// No need to explicit check, compiler does it for you.
-	ifcinteger k = superset.Degree;
+	ifcinteger k = superset.Degree2;
 	ifclogical b = superset.SelfIntersect;
-	int x = superset.Degree;
+	//int x = superset.Degree2;
 
-	cout << "Superset Version : " << display(superset.version) << endl;
-	cout << "Value of x is : " << x << endl;
+	//cout << "Value of x is : " << x << endl;
 	cout << "Value of k is : " << k << endl;
 	cout << "Value of b is : " << b << endl;
 
